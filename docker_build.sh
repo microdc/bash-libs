20f51ff2d11a000b6568745c7466606c1544676b
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2018-09-18 21:33:35 +0100

    Fix styling violations reported by shellcheck

--- docker_build.sh
+++ docker_build.sh
@@ -5,12 +5,12 @@ default_build () {
   export VERSION=$2
   export CONTAINER_REGISTRY=$3
 
-  if [ -z $APP_NAME ]; then
+  if [ -z "$APP_NAME" ]; then
     log "Application name must be specified as first argument"
     exit 1
   fi
 
-  if [ -z $VERSION ]; then
+  if [ -z "$VERSION" ]; then
     log "Application version must be specified as second argument"
     exit 1
   fi
@@ -23,8 +23,8 @@ default_build () {
   log "Building $APP_NAME with version $VERSION. Tasks: $GRADLE_TASKS"
 
   if [ -n "$CONTAINER_REGISTRY" ]; then
-      docker build --build-arg GRADLE_TASKS="${GRADLE_TASKS}" --build-arg VERSION=${VERSION} --build-arg CONTAINER_REGISTRY=${CONTAINER_REGISTRY} --no-cache --rm -t ${APP_NAME}:${VERSION} -t ${APP_NAME}:latest .
+      docker build --build-arg GRADLE_TASKS="${GRADLE_TASKS}" --build-arg VERSION="${VERSION}" --build-arg CONTAINER_REGISTRY="${CONTAINER_REGISTRY}" --no-cache --rm -t "${APP_NAME}:${VERSION}" -t "${APP_NAME}:latest" .
   else
-      docker build --build-arg GRADLE_TASKS="${GRADLE_TASKS}" --build-arg VERSION=${VERSION} --no-cache --rm -t ${APP_NAME}:${VERSION} -t ${APP_NAME}:latest .
+      docker build --build-arg GRADLE_TASKS="${GRADLE_TASKS}" --build-arg VERSION="${VERSION}" --no-cache --rm -t "${APP_NAME}:${VERSION}" -t "${APP_NAME}:latest" .
   fi
 }
